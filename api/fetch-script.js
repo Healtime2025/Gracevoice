155a111eada5706373e0237214d90fe402ca995f
Healtime2025 <smoriti@gmail.com>
2025-05-02 04:48:28 +0200

    Create fetch-script.js

---
+++ api/fetch-script.js
@@ -0,0 +1,13 @@
+export default async function handler(req, res) {
+  const { book, chapter } = req.query;
+  const SCRIPT_URL = "https://script.google.com/macros/s/AKfycbzntpRTmAaDZSP3LJn1X-3nsJeyAr6AMPGg41Du9_Hnh0XFsNwI0o2O6hUVEzEFeT529Q/exec";
+
+  try {
+    const response = await fetch(`${SCRIPT_URL}?book=${book}&chapter=${chapter}`);
+    const data = await response.json();
+    res.setHeader("Access-Control-Allow-Origin", "*");
+    res.status(200).json(data);
+  } catch (err) {
+    res.status(500).json({ error: "GraceVoice Proxy Error", details: err.message });
+  }
+}
